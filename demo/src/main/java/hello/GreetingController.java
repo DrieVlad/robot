8440c533f6e04195eef21ed7c6e1d204f74a7b26
DrieVlad <43147263+DrieVlad@users.noreply.github.com>
2019-04-10 16:02:41 +0500

    Add files via upload

--- demo/src/main/java/hello/GreetingController.java
+++ demo/src/main/java/hello/GreetingController.java
@@ -1,20 +1,39 @@
 package hello;
 
+import gui.GameVisualizer;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
+
 @Controller
 public class GreetingController {
 
 
+    public volatile double m_robotPositionX;
+    public volatile double m_robotPositionY;
+    public volatile double m_robotDirection;
+
+    public volatile int m_targetPositionX;
+    public volatile int m_targetPositionY;
+
+    public volatile double currentWidth;
+    public volatile double currentHeight;
+    GameVisualizer game;
+
+
+    GreetingController(){
+        this.game = new GameVisualizer();
+    }
+
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Greeting greeting(HelloMessage message) throws Exception {
-        Thread.sleep(1000); // simulated delay
-        //System.out.println("gj");
-        return new Greeting(HtmlUtils.htmlEscape(message.getName()));
+        //Thread.sleep(1000); // simulated delay
+        String s = game.Back(message.getName());
+
+        return new Greeting(HtmlUtils.htmlEscape(s));
     }
 
 }
